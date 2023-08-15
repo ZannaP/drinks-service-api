01170ba445b76ecffe0eb99bcba4f22e7ee17b1d
zpran <zanna@prane.org>
2023-08-15 17:45:04 +0100

    feat: test and GET for coffeelover
    Implement GET request for coffeelover endpoint and test

--- src/controllers/home_request.test.ts
+++ src/controllers/home_request.test.ts
@@ -5,7 +5,11 @@ describe("Test home API endpoint request", () => {
   test("GET should return correct message", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toEqual(200);
-    // expect(res.text).toEqual("Welcome to the Drinks API!");
-    expect(res.text).toEqual("This should fail!");
+    expect(res.text).toEqual("Welcome to the Drinks API!");
+  });
+  test("GET should return correct message to the /coffeelover - I like coffee!", async () => {
+    const res = await request(app).get("/coffeelover");
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toEqual("I like coffee!");
   });
 });

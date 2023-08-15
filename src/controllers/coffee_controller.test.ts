04bbbba0647d9bf1defce91714dda472e52d7dba
zpran <zanna@prane.org>
2023-08-15 21:25:58 +0100

    test: added coffee controller tests
    tests with a different coffee name and the coffee name parameter is not specified

--- src/controllers/coffee_controller.test.ts
+++ src/controllers/coffee_controller.test.ts
@@ -1,14 +1,30 @@
 import request from "supertest";
 import { app } from "../app";
-describe("Test home API endpoint request", () => {
-  test("GET /coffee should return correct object", async () => {
-    const res = await request(app)
-      .get("/coffee")
-      .query({ coffeeName: "Latte" });
+describe("GET /coffee should return correct object", () => {
+  test("GET /coffee should return Latte by default", async () => {
+    const res = await request(app).get("/coffee");
     expect(res.statusCode).toEqual(200);
     expect(res.body).toEqual({
       drinkType: "Coffee",
       name: "Latte",
+    });
+  });
+  test("GET /coffee should return name from parameter", async () => {
+    const res = await request(app)
+      .get("/coffee")
+      .query({ coffeeName: "Decaf" });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+      drinkType: "Coffee",
+      name: "Decaf",
+    });
+  });
+  test("GET /coffee should return name from parameter", async () => {
+    const res = await request(app).get("/coffee?coffeeName=Espresso");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+      drinkType: "Coffee",
+      name: "Espresso",
     });
   });
 });
